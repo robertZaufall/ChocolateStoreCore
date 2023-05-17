2efd27bc549fb21bb31a596a84bf5032e1bff828
mail@zaufall.de <mail@zaufall.de>
2023-05-17 23:19:10 +0200

    add sbom tool and result

---
+++ sbom.ps1
@@ -0,0 +1,2 @@
+﻿dotnet tool install --global Microsoft.Sbom.DotNetTool
+sbom-tool generate -b .\ChocolateStoreCore\bin\Release\net6.0\win-x86 -bc .\ChocolateStoreCore -pn ChocolateStoreCore -pv 1.0.0 -ps RobertZaufall -nsb https://sbom.zaufall.de -m .\.sbom
