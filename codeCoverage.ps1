08959e79c9460c0c6f177c1f74f61f25ce5475f4
Rob <mail@zaufall.de>
2023-05-29 04:21:10 +0200

    remove unnecessary code

--- codeCoverage.ps1
+++ codeCoverage.ps1
@@ -1,45 +1,15 @@
-﻿# PURPOSE: Automates the running of Unit Tests and Code Coverage
-# REF: https://stackoverflow.com/a/70321555/495455
-
-# If running outside the test folder
-#cd E:\Dev\XYZ\src\XYZTestProject
-
-# This only needs to be installed once (globally), if installed it fails silently: 
-dotnet tool install -g dotnet-reportgenerator-globaltool
-
-# Save currect directory into a variable
+﻿dotnet tool install -g dotnet-reportgenerator-globaltool
 $dir = "$pwd\ChocolateStoreCoreTests"
+$license = [System.Environment]::GetEnvironmentVariable('license', 'Machine')
 
-# Delete previous test run results (there's a bunch of subfolders named with guids)
-Remove-Item -Recurse -Force $dir/TestResults/
-
-# Run the Coverlet.Collector (this is an NuGet included with XUnit Test Projects)
-$output = [string] (& dotnet test --collect:"XPlat Code Coverage" 2>&1)
-Write-Host "Last Exit Code: $lastexitcode"
-Write-Host $output
-
-# Extract the GUID from the Output eg, 
-$i = $output.LastIndexOf("TestResults") + 11
-$j = $output.LastIndexOf("coverage")
-$cmdGuid = $output.SubString($i,$j - $i - 1)
-Write-Host $cmdGuid 
-
-# Delete previous test run reports - note if you're getting wrong results do a Solution Clean and Rebuild to remove stale DLLs in the bin folder
 Remove-Item -Recurse -Force $dir/coveragereport/
-
-# To keep a history of the Code Coverage we need to use the argument:
-# -historydir:SOME_DIRECTORY 
+Remove-Item -Recurse -Force $dir/TestResults/
 if (!(Test-Path -path $dir/CoverageHistory)) {  
   New-Item -ItemType directory -Path $dir/CoverageHistory
 }
 
-# get ReportGenerator license if available (has to be set manually)
-$license = [System.Environment]::GetEnvironmentVariable('license', 'Machine')
+dotnet test --collect:"XPlat Code Coverage"
+reportgenerator -reports:"$dir\**\coverage.cobertura.xml" -targetdir:"$dir\coveragereport" -reporttypes:Html -historydir:$dir/CoverageHistory -license:"$license"
 
-# Generate the Code Coverage HTML Report
-$reports = ("$dir\TestResults\$cmdGuid\coverage.cobertura.xml").Replace('\\', '\')
-reportgenerator -reports:"$reports" -targetdir:"$dir\coveragereport" -reporttypes:Html -historydir:$dir/CoverageHistory -license:"$license"
-
-# Open the Code Coverage HTML Report (if running on a WorkStation)
 $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
 (& "$dir/coveragereport/index.html")
