6bbc3e070890461752100c63fe418466a5caf8cd
Rob <mail@zaufall.de>
2023-06-11 18:09:34 +0200

    refactor publish-script

--- publish.ps1
+++ publish.ps1
@@ -1,18 +1,25 @@
-$dir = "$pwd\ChocolateStoreCore"
-$dirPackageBinaries = "$pwd\ChocolateyPackages\chocolatestore\tools"
-$dirPackageNuspec = "$pwd\ChocolateyPackages\chocolatestore\chocolatestore.nuspec"
-$dirRelease = "$pwd\release"
-$dirReleaseAssets = "$pwd\release\ChocolateStoreCore"
-$zip = 'ChocolateStoreCore.zip'
+$root = "$pwd"
+
+$dir = "$root\ChocolateStoreCore"
 $projectFile = "$dir\ChocolateStoreCore.csproj"
-$installps1 = "$pwd\ChocolateyPackages\chocolatestore\tools\chocolateyInstall.ps1"
+
+$dirRelease = "$root\release"
+$dirReleaseAssets = "$dirRelease\ChocolateStoreCore"
+$dirReleaseAssetsStore = "$dirReleaseAssets\store"
+
+$dirPackage = "$root\ChocolateyPackages\chocolatestore"
+$dirPackageNuspec = "$dirPackage\chocolatestore.nuspec"
+$dirPackageBinaries = "$dirPackage\tools"
+$installps1 = "$dirPackageBinaries\chocolateyInstall.ps1"
+
+$zip = 'ChocolateStoreCore.zip'
 
 If (Test-Path $dirRelease) { Remove-Item $dirRelease -Recurse -Force }
 
 dotnet publish $projectFile -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishDir="$dirReleaseAssets"
 
-New-Item -ItemType Directory -Path (Join-Path $dirReleaseAssets 'store')
-New-Item -Path (Join-Path -Path (Join-Path -Path $dirReleaseAssets -ChildPath 'store') -ChildPath 'dummy.txt') -ItemType File
+New-Item -ItemType Directory -Path $dirReleaseAssetsStore
+New-Item -Path (Join-Path -Path $dirReleaseAssetsStore -ChildPath 'dummy.txt') -ItemType File
 
 Compress-Archive -Path "$dirReleaseAssets\*" -DestinationPath (Join-Path $dirRelease $zip)
 Copy-Item (Join-Path $dirRelease $zip) -Destination (Join-Path $dirPackageBinaries $zip) -Force
@@ -23,3 +30,13 @@ $newContent = $content -replace "(?<=checksum\s*=\s*')[^']*", $hash
 $newContent | Set-Content -Path $installps1
 
 choco pack $dirPackageNuspec --outdir $dirRelease
+
+If (Test-Path (Join-Path $dirPackageBinaries $zip)) {
+    Remove-Item (Join-Path $dirPackageBinaries $zip) -Force 
+}
+
+[xml]$nuspecContent = Get-Content $dirPackageNuspec
+$version = $nuspecContent.package.metadata.version.ToString()
+$package = "$dirRelease\chocolatestore.$version.nupkg"
+$ziparchive = (Join-Path $dirRelease $zip)
+
