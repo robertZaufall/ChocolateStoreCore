0dfeb33691839382a4b63676fea133f4873635a8
Rob <mail@zaufall.de>
2024-07-11 23:34:34 +0200

    update package installation script [skip actions]

--- ChocolateyPackages/chocolatestore/tools/chocolateyInstall.ps1
+++ ChocolateyPackages/chocolatestore/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   packageName    = $packageName
   filefullpath   = $fileFullPath
   destination    = $destination
-  checksum       = 'AC5236182D756797DC7E331CDFAE62A1BF18DA951DAC7935D11EBC1192A5C75E'
+  checksum       = '2764745D7CEFC6F0C85532FD992F74816525222B7F4CAC3CF0D1754944B4CDC1'
   checksumType   = 'sha256'                
 }
 
@@ -25,6 +25,15 @@ Get-ChocolateyUnzip @packageArgs
 
 # restore
 $filesToBackup | %{ if(Test-Path (Join-Path $destination $_[1])) { Copy-Item (Join-Path $destination $_[1]) -Destination (Join-Path $destination $_[0]) -Force } }
+
+# V1.1.0: patch appsettings.json
+if(Test-Path (Join-Path $destination "appsettings.json")) {
+    $oldString = '"ApiPackageRequestWithVersion": "/Packages()?$filter=(tolower(Id)%20eq%20''{0}'')%20and%20version%20eq%20''{1}''"'
+    $newString = '"ApiPackageRequestWithVersion": "/Packages()?$filter=(tolower(Id)%20eq%20''{0}'')%20and%20(Version%20eq%20''{1}'')"' 
+    $appsettings = Get-Content (Join-Path $destination "appsettings.json") -Raw
+    $appsettings = $appsettings -replace [regex]::Escape($oldString), $newString
+    $appsettings | Set-Content (Join-Path $destination "appsettings.json")
+}
 
 $destinationLink   = Join-Path 'C:\Users\Public\Desktop' 'ChocolateStore.lnk'
 Install-ChocolateyShortcut -ShortcutFilePath $destinationLink -TargetPath $destination
